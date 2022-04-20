ca8b3129726e82f1ec02db98304dfa0a86d36ca6
Ashfak Uddin <ashmanpapet@gmail.com>
2022-04-19 22:48:54 -0400

    auto down?

--- src/list.jsx
+++ src/list.jsx
@@ -23,6 +23,7 @@ class List extends React.Component
 		});
 		client.on('update', (message) => {
 			console.log(message);
+			document.getElementById("mgL").scrollTop = document.getElementById("mgL").scrollHeight;
 			this.setState({list: message});
 		});
 	}
@@ -44,7 +45,7 @@ class List extends React.Component
 		let i = 0;	
 		return ( 
 				<div>
-				<div className = "list">
+				<div className = "list"d = "mgL">
 					<Setup />{this.state.list.map((e) => <Message info = {e} key = {i++} />)}
 				</div>
 				<Send client = {this.props.client} username = {this.props.username} />
